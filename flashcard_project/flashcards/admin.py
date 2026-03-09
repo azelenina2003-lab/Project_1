9dd4677bcadd1714424cb7d40b8a07f49f936010
Anastasia Zelenina <azelenina2003@gmail.com>
2026-03-09 11:55:02 +0300

    registrasy admin

--- flashcard_project/flashcards/admin.py
+++ flashcard_project/flashcards/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Deck, Card
+
+@admin.register(Deck)
+class DeckAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('term', 'deck', 'created_at')
+    list_filter = ('deck',)
+    search_fields = ('term', 'definition')
