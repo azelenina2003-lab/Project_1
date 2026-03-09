658d3c2cabadd81fcc2139fd58de62fdb72afdfb
Anastasia Zelenina <azelenina2003@gmail.com>
2026-03-09 12:21:05 +0300

    add new funcial

--- flashcard_project/flashcards/views.py
+++ flashcard_project/flashcards/views.py
@@ -15,3 +15,26 @@ def deck_detail(request, pk):
     deck = get_object_or_404(Deck, pk=pk)
     cards = deck.cards.all()  
     return render(request, 'flashcards/deck_detail.html', {'deck': deck, 'cards': cards})
+
+# Создание новой колоды
+def deck_create(request):
+    if request.method == 'POST':
+        form = DeckForm(request.POST)
+        if form.is_valid():
+            deck = form.save()
+            return redirect('deck_detail', pk=deck.pk)
+    else:
+        form = DeckForm()
+    return render(request, 'flashcards/deck_form.html', {'form': form, 'title': 'Новая колода'})
+
+# Редактирование колоды
+def deck_update(request, pk):
+    deck = get_object_or_404(Deck, pk=pk)
+    if request.method == 'POST':
+        form = DeckForm(request.POST, instance=deck)
+        if form.is_valid():
+            deck = form.save()
+            return redirect('deck_detail', pk=deck.pk)
+    else:
+        form = DeckForm(instance=deck)
+    return render(request, 'flashcards/deck_form.html', {'form': form, 'title': 'Редактировать колоду'})
